62aca694201804ac99a26a5bee89004e80b6476f
Ahmed Abdulrahim <medo1562003@gmail.com>
2024-10-13 01:16:44 +0300

    resolved merge conflicts errors 2

--- LearnFlow/obj/Debug/net8.0/LearnFlow.AssemblyInfo.cs
+++ LearnFlow/obj/Debug/net8.0/LearnFlow.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LearnFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2359f83a9591b02de9719a1ee26ca6bbfdad8052")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d376a9c89507db104f81a97f4554fcefe5276cff")]
 [assembly: System.Reflection.AssemblyProductAttribute("LearnFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LearnFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
