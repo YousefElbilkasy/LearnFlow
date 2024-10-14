27da3fff61c188d2a6458b7f79b56c7941d23e1e
Ahmed Abdulrahim <medo1562003@gmail.com>
2024-10-14 23:17:21 +0300

    Edited Login and Register methods in Account Controller

--- LearnFlow/Controllers/AccountController.cs
+++ LearnFlow/Controllers/AccountController.cs
@@ -6,88 +6,99 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace LearnFlow.Controllers
 {
-  public class AccountController : Controller
-  {
-    public readonly UserManager<User> UserManager;
-    public readonly SignInManager<User> signInManager;
-
-    public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+    public class AccountController : Controller
     {
-      this.UserManager = userManager;
-      this.signInManager = signInManager;
-    }
+        public readonly UserManager<User> UserManager;
+        public readonly SignInManager<User> signInManager;
 
-    // GET: AccountController
-    [HttpGet]
-    public ActionResult Register()
-    {
-      return View();
-    }
-
-    [HttpPost]
-    public async Task<IActionResult> SaveRegister(RegisterUserViewModel model)
-    {
-      // Save Image
-      if (model.ImageUrl != null)
-      {
-        var fileName =
-        Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\uploads\\users", model.ImageUrl.FileName);
-        model.ImageUrl.CopyTo(new FileStream(fileName, FileMode.Create));
-      }
-
-      if (ModelState.IsValid)
-      {
-        var user = new User
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
         {
-          FullName = model.FullName,
-          UserName = model.UserName,
-          Email = model.Email,
-          PhoneNumber = model.PhoneNumber,
-          Role = model.Role,
-          ImageUrl = model.ImageUrl?.FileName ?? "default.png",
-          DateJoined = DateTime.Now,
-          PasswordHash = model.Password
-        };
-
-        var result = await UserManager.CreateAsync(user, model.Password);
-
-        if (result.Succeeded)
-        {
-          await signInManager.SignInAsync(user, false);
-          return RedirectToAction("LogIn");
+            this.UserManager = userManager;
+            this.signInManager = signInManager;
         }
 
-        foreach (var error in result.Errors)
-          ModelState.AddModelError("", error.Description);
-      }
-      return View("Register", model);
+        // GET: AccountController
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveRegister(RegisterUserViewModel model)
+        {
+            // Save Image
+            if (model.ImageUrl != null)
+            {
+                var fileName =
+                Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\uploads\\users", model.ImageUrl.FileName);
+                model.ImageUrl.CopyTo(new FileStream(fileName, FileMode.Create));
+            }
+
+            if (ModelState.IsValid)
+            {
+                var user = new User
+                {
+                    FullName = model.FullName,
+                    UserName = model.UserName,
+                    Email = model.Email,
+                    PhoneNumber = model.PhoneNumber,
+                    Role = model.Role,
+                    ImageUrl = model.ImageUrl?.FileName ?? "default.png",
+                    DateJoined = DateTime.Now,
+                    PasswordHash = model.Password
+                };
+
+                var result = await UserManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, false);
+                    return RedirectToAction("LogIn");
+                }
+
+                foreach (var error in result.Errors)
+                    ModelState.AddModelError("", error.Description);
+            }
+            return View("Register", model);
+        }
+
+        [HttpGet]
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveLogIn(LogInUserViewModel loginmodel)
+        {
+            if (ModelState.IsValid)
+            {
+                var emailfound = await UserManager.FindByEmailAsync(loginmodel.Email);
+                if (emailfound != null)
+                {
+                    bool isPasswordValid = await UserManager.CheckPasswordAsync(emailfound, loginmodel.Password);
+                    if (isPasswordValid)
+                    {
+                        var result = await signInManager.PasswordSignInAsync(emailfound, loginmodel.Password, false, false);
+                        if (result.Succeeded) return RedirectToAction("Student", "Index");
+                        TempData["Error"] = "Login Failed, please try again later";
+                        return View(loginmodel);
+                    }
+                    TempData["Error"] = "Wrong Credentials, please try again";
+                    return View(loginmodel);
+                }
+                TempData["Error"] = "Email not Found";
+                return View(loginmodel);
+
+            }
+            return View("LogIn", loginmodel);
+        }
+
+        public async Task<IActionResult> LogOut()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("LogIn");
+        }
     }
-
-    [HttpGet]
-    public IActionResult LogIn()
-    {
-      return View();
-    }
-
-    [HttpPost]
-    public async Task<IActionResult> SaveLogIn(LoginViewModel model)
-    {
-      if (ModelState.IsValid)
-      {
-        var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
-        if (result.Succeeded)
-          return RedirectToAction("Index", "Home");
-
-        ModelState.AddModelError("", "Invalid Email or Password");
-
-      }
-      return View("LogIn", model);
-    }
-
-    public async Task<IActionResult> LogOut()
-    {
-      await signInManager.SignOutAsync();
-      return RedirectToAction("LogIn");
-    }
-  }
 }
--- LearnFlow/obj/Debug/net8.0/LearnFlow.AssemblyInfo.cs
+++ LearnFlow/obj/Debug/net8.0/LearnFlow.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LearnFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+598f37426d6225cd50d70547ad6f5103e6d57520")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a903e767b711fe53236a41ccbeb4452a94f1449")]
 [assembly: System.Reflection.AssemblyProductAttribute("LearnFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LearnFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
