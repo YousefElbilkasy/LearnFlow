ceba67478d3210baaeabef3ada8c4e425c12eec9
mena-essam <mena2004.com@gmail.com>
2024-10-16 17:45:43 +0300

    Add files via upload

---
+++ LearnFlow/Interfaces/IEnrollmentRepo.cs
@@ -0,0 +1,9 @@
+﻿using LearnFlow.Models;
+
+public interface IEnrollmentRepo
+{
+    Task<Enrollment?> GetEnrollmentByStudentAndCourseAsync(int studentId, int courseId);
+    Task EnrollStudentAsync(Enrollment enrollment);
+}
+
+
