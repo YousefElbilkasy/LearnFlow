f9f83f4bd6fa31c8ac496439e12986fa04d8c84c
Ahmed Abdulrahim <medo1562003@gmail.com>
2024-10-17 16:02:25 +0300

    fixed QuizRepo

--- LearnFlow/Repository/QuizRepo.cs
+++ LearnFlow/Repository/QuizRepo.cs
@@ -6,49 +6,49 @@ using Microsoft.EntityFrameworkCore;
 
 namespace LearnFlow.Repository
 {
-  public class QuizRepo : IRepo<Quiz>
-  {
-    private readonly LearnFlowContext context;
-
-    public QuizRepo(LearnFlowContext context)
+    public class QuizRepo : IRepo<Quiz>
     {
-      this.context = context;
-    }
+        private readonly LearnFlowContext context;
 
-    public async Task<Quiz> CreateAsync(Quiz entity)
-    {
-      context.Quizs.Add(entity);
-      await context.SaveChangesAsync();
-      return entity;
-    }
+        public QuizRepo(LearnFlowContext context)
+        {
+            this.context = context;
+        }
 
-    public async Task<Quiz> DeleteAsync(int id)
-    {
-      var quiz = await context.Quizs.FindAsync(id);
-      if (quiz == null)
-      {
-        return null;
-      }
-      context.Quizs.Remove(quiz);
-      await context.SaveChangesAsync();
-      return quiz;
-    }
+        public async Task<Quiz> CreateAsync(Quiz entity)
+        {
+            context.Quizs.Add(entity);
+            await context.SaveChangesAsync();
+            return entity;
+        }
 
-    public async Task<IEnumerable<Quiz>> GetAllAsync()
-    {
-      return await context.Quizs.ToListAsync();
-    }
+        public async Task<Quiz> DeleteAsync(int id)
+        {
+            var quiz = await context.Quizs.FindAsync(id);
+            if (quiz == null)
+            {
+                return null;
+            }
+            context.Quizs.Remove(quiz);
+            await context.SaveChangesAsync();
+            return quiz;
+        }
 
-    public async Task<Quiz> GetByIdAsync(int id)
-    {
-      return await context.Quizs.FindAsync(id);
-    }
+        public async Task<IEnumerable<Quiz>> GetAllAsync()
+        {
+            return await context.Quizs.ToListAsync();
+        }
 
-    public async Task<Quiz> UpdateAsync(Quiz entity)
-    {
-      context.Quizs.Update(entity);
-      await context.SaveChangesAsync();
-      return entity;
+        public async Task<Quiz> GetByIdAsync(int id)
+        {
+            return await context.Quizs.FindAsync(id);
+        }
+
+        public async Task<Quiz> UpdateAsync(Quiz entity)
+        {
+            context.Quizs.Update(entity);
+            await context.SaveChangesAsync();
+            return entity;
+        }
     }
-  }
 }
