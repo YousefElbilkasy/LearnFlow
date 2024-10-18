800fb5d0ca6428abf561f24face74b483b4474b7
Shahd Mohammed <shhdmm932@gmail.com>
2024-10-18 05:11:28 +0300

    astr ya rab

--- LearnFlow/obj/Debug/net8.0/LearnFlow.AssemblyInfo.cs
+++ LearnFlow/obj/Debug/net8.0/LearnFlow.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LearnFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d29196e03a5cc4ef6e79f293dda0eae5f6ffa3f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LearnFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LearnFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
